0262075d814ec3a5668d568d865ad1e9fe1c94f0
Denis <lorddionis@gmail.com>
2023-03-20 16:52:40 -0700

    commit

---
+++ src/main/java/dev/den/controller/TaskController.java
@@ -0,0 +1,63 @@
+package dev.den.controller;
+
+import dev.den.domain.Task;
+import dev.den.service.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Objects.isNull;
+
+@Controller
+@RequestMapping("/")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/")
+    public String tasks(Model model,
+                        @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                        @RequestParam(value = "limit", required = false, defaultValue = "10") int limit) {
+        List<Task> tasks = taskService.getAll((page - 1) * limit, limit);
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("current_pageNumber", page);
+        int totalPage = (int) Math.ceil(1.0 * taskService.getAllCount() / limit);
+        if (totalPage > 1) {
+            List<Integer> pageNumber = IntStream.rangeClosed(1, totalPage).boxed().collect(Collectors.toList());
+            model.addAttribute("page_number", pageNumber);
+        }
+        return "tasks";
+    }
+
+    @PostMapping("/{id}")
+    public String edit(Model model, @PathVariable Integer id, @RequestBody TaskInfo info) {
+        if (isNull(id) || id <= 0) {
+            throw new RuntimeException("invalid id");
+        }
+        Task task = taskService.edit(id, info.getDescription(), info.getStatus());
+        return tasks(model, 1, 10);
+    }
+
+    @PostMapping("/")
+    public String add(Model model, @RequestBody TaskInfo info) {
+        Task task = taskService.create(info.getDescription(), info.getStatus());
+        return tasks(model, 1, 10);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(Model model, @PathVariable Integer id) {
+        if (isNull(id) || id <= 0) {
+            throw new RuntimeException("invalid id");
+        }
+        taskService.delete(id);
+        return tasks(model, 1, 10);
+    }
+}
---
+++ src/main/java/dev/den/domain/Status.java
@@ -0,0 +1,7 @@
+package dev.den.domain;
+
+public enum Status {
+    IN_PROGRESS,
+    DONE,
+    PAUSED
+}
